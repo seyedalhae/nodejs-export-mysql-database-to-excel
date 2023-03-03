878500fb567e806793d4669c9e3cd2796cf24a6c
Alireza <77020648+seyedalhae@users.noreply.github.com>
2023-03-03 18:36:51 +0330

    Commenting

--- src/index.js
+++ src/index.js
@@ -9,28 +9,46 @@ const db = mysql.createConnection({
 	database: "test",
 });
 
+// Open the MySQL connection
 db.connect((err) => {
 	if (err) throw err;
 
-	db.query("SELECT * FROM users", function (err, users, fields) {
+	// Query data from MySQL
+	db.query("SELECT * FROM users", (err, users, fields) => {
 		const jsonUsers = JSON.parse(JSON.stringify(users));
 		console.log(jsonUsers);
+		/**
+			The database information is like the following array
+			[
+				{ id: 1, name: 'Ali', age: 21, city: 'Tehran' },
+				{ id: 2, name: 'Reza', age: 22, city: 'Tabriz' },
+				{ id: 3, name: 'Diana', age: 23, city: 'Mazandaran' },
+				{ id: 4, name: 'Mahdi', age: 24, city: 'Tehran' },
+				{ id: 5, name: 'Sara', age: 25, city: 'Alborz' }
+			]
+		*/
 
+		// Creating workbook
 		let workbook = new excel.Workbook();
+		// Creating worksheet
 		let worksheet = workbook.addWorksheet("users");
 
+		// WorkSheet Header
 		worksheet.columns = [
 			{ header: "Id", key: "id", width: 10 },
 			{ header: "Name", key: "name", width: 30 },
 			{ header: "Price", key: "price", width: 30 },
 		];
 
+		// Add rows to the Excel file
 		worksheet.addRows(jsonUsers);
 
+		// Output from Excel file
 		workbook.xlsx
 			.writeFile("users.xlsx")
 			.then(() => console.log("file saved!"));
 
+		// Close MySQL connection
 		db.end((err) => {
 			if (err) {
 				return console.log("error:" + err.message);

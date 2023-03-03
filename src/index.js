29e2d431cb54978dd7189540311c71d6354edd3c
Alireza <77020648+seyedalhae@users.noreply.github.com>
2023-03-03 16:18:00 +0330

    Completing the conversion of the database to Excel

--- src/index.js
+++ src/index.js
@@ -9,3 +9,33 @@ const db = mysql.createConnection({
 	database: "test",
 });
 
+db.connect((err) => {
+	if (err) throw err;
+
+	db.query("SELECT * FROM users", function (err, users, fields) {
+		const jsonUsers = JSON.parse(JSON.stringify(users));
+		console.log(jsonUsers);
+
+		let workbook = new excel.Workbook();
+		let worksheet = workbook.addWorksheet("users");
+
+		worksheet.columns = [
+			{ header: "Id", key: "id", width: 10 },
+			{ header: "Name", key: "name", width: 30 },
+			{ header: "Price", key: "price", width: 30 },
+		];
+
+		worksheet.addRows(jsonUsers);
+
+		workbook.xlsx
+			.writeFile("users.xlsx")
+			.then(() => console.log("file saved!"));
+
+		db.end((err) => {
+			if (err) {
+				return console.log("error:" + err.message);
+			}
+			console.log("Close the database connection.");
+		});
+	});
+});

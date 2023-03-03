06c9be12dea6b6d1cb98368d41d9ddebb0a3914d
Alireza <77020648+seyedalhae@users.noreply.github.com>
2023-03-03 20:35:39 +0330

    Customizable table name

--- src/config.js
+++ src/config.js
@@ -5,14 +5,21 @@ const config = {
 		user: "root",
 		password: "",
 		database: "test",
+		table: "users",
 	},
-	
+
 	// Enter the names of the database columns as well as Excel as below
 	dbColumns: [
 		{ header: "Id", key: "id", width: 10 },
 		{ header: "Name", key: "name", width: 30 },
 		{ header: "Price", key: "price", width: 30 },
 	],
+
+	// Write the names you want for Excel
+	excelDetail: {
+		fileName: "Users",
+		sheetName: "users",
+	},
 };
 
 module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const excel = require("exceljs");
-const {dbConfig, dbColumns} = require('./config');
+const { dbConfig, dbColumns, excelDetail } = require("./config");
 
 // Create a connection to the database
 const db = mysql.createConnection(dbConfig);
@@ -10,24 +10,14 @@ db.connect((err) => {
 	if (err) throw err;
 
 	// Query data from MySQL
-	db.query("SELECT * FROM users", (err, users, fields) => {
+	db.query(`SELECT * FROM ${dbConfig.table}`, (err, users, fields) => {
 		const jsonUsers = JSON.parse(JSON.stringify(users));
 		console.log(jsonUsers);
-		/**
-			The database information is like the following array
-			[
-				{ id: 1, name: 'Ali', age: 21, city: 'Tehran' },
-				{ id: 2, name: 'Reza', age: 22, city: 'Tabriz' },
-				{ id: 3, name: 'Diana', age: 23, city: 'Mazandaran' },
-				{ id: 4, name: 'Mahdi', age: 24, city: 'Tehran' },
-				{ id: 5, name: 'Sara', age: 25, city: 'Alborz' }
-			]
-		*/
 
 		// Creating workbook
 		let workbook = new excel.Workbook();
 		// Creating worksheet
-		let worksheet = workbook.addWorksheet("users");
+		let worksheet = workbook.addWorksheet(excelDetail.sheetName);
 
 		worksheet.columns = [...dbColumns];
 
@@ -36,7 +26,7 @@ db.connect((err) => {
 
 		// Output from Excel file
 		workbook.xlsx
-			.writeFile("Users.xlsx")
+			.writeFile(`${__dirname}/upload/${excelDetail.fileName}.xlsx`)
 			.then(() => console.log("File saved!"));
 
 		// Close MySQL connection
